be05d0fc2679295bdad17d5944d2168c29a78502
AtharvAwle <atharvawle0156@gmail.com>
2024-07-26 20:15:02 +0530

    26 july practices array medium

---
+++ Arrays/Medium.java
@@ -0,0 +1,28 @@
+public class Medium {
+    public static void main(String[] args) {
+
+    }
+
+
+//    MAXIMUM SUBARRAY SUM
+//    KADANES ALGORITHM
+public static long maxSubarraySum(int[] arr, int n) {
+    // write your code here
+    long cs = 0;
+    long ms = Integer.MIN_VALUE;
+
+    for(int i = 0 ; i < n ; i++){
+        cs = cs + arr[i];
+        //check if current sum is greater than max sum the make the maxsum the current sum
+        if(cs > ms){
+            ms = cs;
+        }
+        //If current sum is negative make it 0
+        if(cs < 0){
+            cs = 0;
+        }
+
+    }
+    return ms;
+}
+}
